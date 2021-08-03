04786bf88891b0ffa6555a3f0d689d94f587e5c3
Erminio <Erminio>
2021-08-03 02:36:44 +0000

    Redirect After Form Submission - Redirect Short URLs  express_server.js

--- express_server.js
+++ express_server.js
@@ -8,10 +8,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//form via POST é enviado via buffer e parser make readable for us humans
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function generateRandomString() {
+  const length = 6;
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let x = 1; x <= length; x++) {
+    randomString += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return randomString;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -22,9 +31,8 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//Sending HTML
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 //new route handler for "/urls" and use res.render() to pass the URL data to our template.
@@ -32,8 +40,33 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
 //pego parametro(valor) q veio da urls req.params.shortURL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  let newKey = generateRandomString();
+  //console.log(req.body.longURL);              // Log the POST request body to the console
+  urlDatabase[`${newKey}`] = req.body.longURL;  // add newKey and url in urlDatabase
+  res.redirect(`/urls/${newKey}`);              // Redirect to the new
+});
+
+
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
